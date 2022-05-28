78a4c5e24c75bf3fd9e8e94d3f203aa9871a0aee
Azarow <contactazarox@gmail.com>
2022-05-28 13:09:42 +0200

    :heavy_plus_sign: add tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Navbar } from '../components/Navbar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <title>Nas-Studio</title>
+    </Head>
+    <Navbar></Navbar>
+    <Component {...pageProps} />
+  </>
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
 
 const Home: NextPage = () => {
   return (
-      <main>
-        <h1>Script component examples</h1>
-        <ul>
-          <li>
-            <Link href="/test">
-              <a>Test</a>
-            </Link>
-          </li>
-        </ul>
-      </main>
+      <div>
+        <div>Hello World</div>
+      </div>
   )
 }
 

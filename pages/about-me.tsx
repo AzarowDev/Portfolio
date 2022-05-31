112c83c5d472f76c7d9b6b4be6124391f78ce922
Azarow <contactazarox@gmail.com>
2022-05-31 23:02:38 +0200

    :construction: add about page et service

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -3,11 +3,71 @@ import React, { useState } from 'react'
 import { AnimatePresence, motion } from 'framer-motion'
 import Transitions from '../components/transitions'
 
-const test = [
-    { id: '1', label: 'test', descrition: 'test'},
-    { id: '2', label: 'test', descrition: 'test'},
-    { id: '3', label: 'test', descrition: 'test'}
-]
+const posts = [
+    {
+      title: 'HTML',
+      category: { name: 'Language Balistique', href: '#' },
+      description:
+        'HyperText Markup Language (HTML) est le code utilisé pour structurer une page web et son contenu. Par exemple, le contenu de votre page pourra être structuré en un ensemble de paragraphes, une liste à puces ou avec des images et des tableaux de données.',
+      imageUrl: 'https://i.postimg.cc/SJ7NYHF1/html.png'
+    },
+    {
+      title: 'CSS',
+      category: { name: 'Language', href: '#' },
+      description:
+        'CSS est un langage basé sur des règles — on définit des règles de styles destinées à des éléments ou des groupes d\'éléments particuliers dans la page. Par exemple, « Je veux que le titre principal de ma page s\'affiche en rouge en gros caractères. » La règle commence par un sélecteur, l\'élément HTML mis en forme.',
+      imageUrl:
+        'https://i.postimg.cc/c63J0rDS/css.png',
+    },
+    {
+      title: 'Git',
+      category: { name: 'VERSION', href: '#' },
+      description:
+        'Git est de loin le système de contrôle de version le plus largement utilisé aujourd\'hui. Git est un projet open source avancé, qui est activement maintenu. À l\'origine, il a été développé en 2005 par Linus Torvalds, le créateur bien connu du noyau du système d\'exploitation Linux.',
+      imageUrl:
+        'https://i.postimg.cc/QHrdthPW/git.png',
+    },
+    {
+        title: 'SASS',
+        category: { name: 'Language', href: '#' },
+        description:
+          'Sass (Syntactically awesome stylesheets) est un langage de script préprocesseur qui est compilé ou interprété en CSS (Cascading Style Sheets). SassScript est le langage de script en lui-même. Sass est disponible en deux syntaxes.',
+        imageUrl:
+          'https://i.postimg.cc/kVZndmKG/sass.png',
+    },
+    {
+      title: 'Typescript',
+      category: { name: 'SUPERSET', href: '#' },
+      description:
+        'TypeScript est un langage open-source pris en charge par Microsoft, qui s\'appuie sur JavaScript en ajoutant une fonction de type statique facultative. Les types fournissent un moyen de structurer et de valider son code avant de l\'exécuter.',
+      imageUrl:
+        'https://i.postimg.cc/FdLhrBVZ/ts.png',
+    },
+    {
+      title: 'React',
+      category: { name: 'BIBLIOTHEQUE', href: '#' },
+      description:
+        'React est une bibliothèque JavaScript utilisée pour construire des composants d\'interface utilisateur réutilisables. Selon la documentation officielle de React, la définition est la suivante - React est une bibliothèque permettant de construire des interfaces utilisateur composables.',
+      imageUrl:
+        'https://i.postimg.cc/210k2v54/react.png',
+    },
+    {
+      title: 'Tailwind',
+      category: { name: 'FRAMEWORK CSS', href: '#' },
+      description:
+        'Tailwind Css est un framework CSS complètement personnalisable, basé sur le principe de classes utilitaires, dont la version 2.0 a été annoncée hier avec encore plus de nouveautés sympathiques. Le site officiel nous annonce la couleur immédiatement : construisez rapidement des sites sans quitter votre code HTML.',
+      imageUrl:
+        'https://i.postimg.cc/gwrY5Jw1/tailwind.png',
+    },
+    {
+      title: 'NodeJS',
+      category: { name: 'RUNTIME', href: '#' },
+      description:
+        'NodeJS est une plateforme construite sur le moteur JavaScript V8 de Chrome qui permet de développer des applications en utilisant du JavaScript. Il se distingue des autres plateformes gràce à une approche non bloquante permettant d\'effectuer des entrées/sorties (I/O) de manière asynchrone.',
+      imageUrl:
+        'https://i.postimg.cc/jDxqHvRx/node.png',
+    },
+  ]
 
 const AboutMe = () => {
     const [selectedId, setSelectedId] = useState(null)
@@ -15,7 +75,41 @@ const AboutMe = () => {
         <div>
             <NextSeo title='About Me' />
             <Transitions>
-            <h1>About me</h1>
+            <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+      <div className="absolute inset-0">
+        <div className="bg-white h-1/3 sm:h-2/3" />
+      </div>
+      <div className="relative max-w-7xl mx-auto">
+        <div className="text-center">
+          <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">Technologies utilisées</h2>
+          <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
+          Tout développeur se doit de maitriser les standards du web mais également d'autres technologies selon sa spécialité.
+          </p>
+        </div>
+        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-4 lg:max-w-none">
+          {posts.map((post) => (
+            <div key={post.title} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+              <div className="flex-shrink-0">
+                <img className="h-48 w-full object-cover" src={ post.imageUrl } alt="" />
+              </div>
+              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1">
+                  <p className="text-sm font-medium text-indigo-600">
+                    <a href={post.category.href} className="hover:underline">
+                    <span className="inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-indigo-100 text-indigo-800"> { post.category.name } </span>
+                    </a>
+                  </p>
+                  <a className="block mt-2">
+                    <p className="text-xl font-semibold text-gray-900">{post.title}</p>
+                    <p className="mt-3 text-base text-gray-500">{post.description}</p>
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
             </Transitions>
         </div>
     )
--- pages/services.tsx
+++ pages/services.tsx
@@ -1,13 +1,122 @@
 import { NextSeo } from 'next-seo'
 import React from 'react'
 import Transitions from '../components/transitions'
+import {
+  CheckIcon,
+} from '@heroicons/react/outline'
+import { ChevronDownIcon } from '@heroicons/react/solid'
+import Link from 'next/link'
+
+function classNames(...classes: any) {
+    return classes.filter(Boolean).join(' ')
+  }
+
+const pricing = {
+    tiers: [
+      {
+        title: 'Application Mobile',
+        price: 'Sur devis uniquement',
+        features: [
+            'Plateform (IOS, Androïd)',
+            'Développement sur mesure',
+            'Performante et fluide',
+            'Mise en production'
+        ],
+        cta: 'Contacter-moi',
+        mostPopular: false,
+      },
+      {
+        title: 'Site internet',
+        price: 'Sur devis uniquement',
+        features: [
+          'Développement sur mesure',
+          'Optimisation du référencelent',
+          'Formation à l\'utilisation de la plateforme',
+        ],
+        cta: 'Contacter-moi',
+        mostPopular: true,
+      },
+      {
+        title: 'Spécifique',
+        price: '€ 150 /jour',
+        features: [
+          'Développement BackEnd',
+          'Développement FrontEnd',
+        ],
+        cta: 'Contacter-moi',
+        mostPopular: false,
+      },
+    ],
+  }
 
 const Services = () => {
     return (
         <div>
             <NextSeo title='Services' />
             <Transitions>
-            <h1>Services</h1>
+            <div className="relative max-w-2xl mx-auto py-24 px-4 sm:px-6 lg:max-w-7xl lg:py-32 lg:px-8">
+          <div className="relative">
+            <h1 className="text-3xl font-extrabold text-gray-900 sm:text-5xl sm:leading-none sm:tracking-tight lg:text-6xl">
+                Plans tarifaires
+            </h1>
+            <p className="mt-6 max-w-2xl text-xl text-gray-500">
+                Choisissez un plan abordable doté des meilleures fonctionnalités
+            </p>
+          </div>
+        </div>
+
+        <main>
+        {/* Pricing Section */}
+        <section className="relative" aria-labelledby="pricing-heading">
+          <h2 id="pricing-heading" className="sr-only">
+            Pricing
+          </h2>
+
+          {/* Tiers */}
+          <div className="max-w-2xl mx-auto px-4 space-y-12 sm:px-6 lg:max-w-7xl lg:space-y-0 lg:px-8 lg:grid lg:grid-cols-3 lg:gap-x-8">
+            {pricing.tiers.map((tier) => (
+              <div
+                key={tier.title}
+                className="relative p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col"
+              >
+                <div className="flex-1">
+                  <h3 className="text-3xl font-semibold text-gray-900">{tier.title}</h3>
+                  {tier.mostPopular ? (
+                    <p className="absolute top-0 py-1.5 px-4 bg-rose-500 rounded-full text-xs font-semibold uppercase tracking-wide text-white transform -translate-y-1/2">
+                      MEILLEUR VENTES
+                    </p>
+                  ) : null}
+                  <p className="mt-4 flex items-baseline text-gray-900">
+                    <span className="text-xl font-extrabold tracking-tight">{tier.price}</span>
+                  </p>
+
+                  {/* Feature list */}
+                  <ul role="list" className="mt-6 space-y-6">
+                    {tier.features.map((feature) => (
+                      <li key={feature} className="flex">
+                        <CheckIcon className="flex-shrink-0 w-6 h-6 text-rose-500" aria-hidden="true" />
+                        <span className="ml-3 text-gray-500">{feature}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+
+                <Link
+                  href="/contact">
+                      <a className={classNames(
+                    tier.mostPopular
+                      ? 'bg-rose-500 text-white hover:bg-rose-600'
+                      : 'bg-rose-50 text-rose-700 hover:bg-rose-100',
+                    'mt-8 block w-full py-3 px-6 border border-transparent rounded-md text-center font-medium'
+                  )}>
+                    {tier.cta}
+                  </a>
+                </Link>
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
             </Transitions>
         </div>
     )
